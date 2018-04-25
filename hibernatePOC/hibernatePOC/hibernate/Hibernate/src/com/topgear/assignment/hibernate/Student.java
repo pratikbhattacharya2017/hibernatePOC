427e5eec160a2dd4e148c124151826efb2a289f3
pratikbhattacharya2017 <pratikbhattacharya2017@yahoo.com>
2018-04-25 23:12:14 +0530

    test all

---
+++ hibernatePOC/hibernatePOC/hibernate/Hibernate/src/com/topgear/assignment/hibernate/Student.java
@@ -0,0 +1,50 @@
+package com.topgear.assignment.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Student")
+public class Student {
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	@Column
+	String firstName;
+	@Column
+	String lastName;
+	@Column
+	String email;
+	
+	
+}
